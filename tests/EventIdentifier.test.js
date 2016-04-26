e4f167e8aa98eb62d2b05987fb6edf0014e3ad33
Alua.Kinzhebayeva <Alua.Kinzhebayeva@gmail.com>
2016-04-26 21:48:59 +0600

    EventIdentifier tests

--- tests/EventIdentifier.test.js
+++ tests/EventIdentifier.test.js
@@ -1,1 +1,36 @@
+'use strict';
 
+import * as chai from 'chai';
+import { expect } from 'chai';
+import { parse } from '../lib/EventIdentifier';
+import * as kinesisEvent from './fixtures/kinesis.json';
+import * as snsEvent from './fixtures/sns.json';
+
+describe('EventIdentifier', () => {
+
+  context('when correct events were provided', () => {
+
+    describe('#parse() Kinesis event', () => {
+      it('returns an array of kinesis objects', (done) => {
+        const messages = parse(kinesisEvent);
+        expect(messages).to.be.an('array');
+        for(let message of messages) {
+          expect(message).to.be.an('object');
+        }
+        done();
+      });
+    });
+
+    describe('#parse() Sns event', () => {
+      it('returns an array of Sns objects', (done) => {
+        const messages = parse(snsEvent);
+        expect(messages).to.be.an('array');
+        for(let message of messages) {
+          expect(message).to.be.an('object');
+        }
+        done();
+      });
+    });
+
+  });
+});
